ab4d0809f6f14403e40261a328ff624675839146
Adithya Jayan <adithyaj8@gmail.com>
2026-04-19 14:15:39 +0530

    Switch from pdfrx to pdfx for F-Droid compatibility

--- lib/screens/pdf_viewer_screen.dart
+++ lib/screens/pdf_viewer_screen.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:pdfrx/pdfrx.dart';
+import 'package:pdfx/pdfx.dart';
 
 /// A screen for viewing PDF documents.
 ///
-/// This screen utilizes the `pdfrx` package to display
+/// This screen utilizes the `pdfx` package to display
 /// PDF files from a given local path.
-class PdfViewerScreen extends StatelessWidget {
+class PdfViewerScreen extends StatefulWidget {
   final String pdfPath; // The local file path of the PDF document to display.
 
   const PdfViewerScreen({super.key, required this.pdfPath});
+
+  @override
+  State<PdfViewerScreen> createState() => _PdfViewerScreenState();
+}
+
+class _PdfViewerScreenState extends State<PdfViewerScreen> {
+  late PdfController _pdfController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pdfController = PdfController(
+      document: PdfDocument.openFile(widget.pdfPath),
+    );
+  }
+
+  @override
+  void dispose() {
+    _pdfController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +37,9 @@ class PdfViewerScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('PDF Viewer'), // Title of the PDF viewer screen.
       ),
-      body: PdfViewer.file(pdfPath), // Display the PDF from the file path.
+      body: PdfView(
+        controller: _pdfController,
+      ),
     );
   }
 }
