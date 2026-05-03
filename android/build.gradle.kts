4920230ef9e4ef849d748d85f03aeee0103edaff
Adithya Jayan <adithyaj8@gmail.com>
2026-05-03 20:05:46 +0530

    Fix JVM target inconsistency by enforcing Java 17 for all subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,28 +16,14 @@ subprojects {
     project.evaluationDependsOn(":app")
     
     tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).configureEach {
-        val java17Projects = setOf("package_info_plus", "share_plus", "wakelock_plus")
-        val java11Projects = setOf(
-            "audio_session",
-            "file_picker",
-            "shared_preferences_android",
-            "path_provider_android",
-            "url_launcher_android"
-        )
-
-        if (java17Projects.contains(project.name)) {
-            kotlinOptions {
-                jvmTarget = "17"
-            }
-        } else if (java11Projects.contains(project.name)) {
-            kotlinOptions {
-                jvmTarget = "11"
-            }
-        } else if (project.name != "app") {
-            kotlinOptions {
-                jvmTarget = "1.8"
-            }
+        kotlinOptions {
+            jvmTarget = "17"
         }
+    }
+
+    tasks.withType(JavaCompile::class.java).configureEach {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
 }
 
