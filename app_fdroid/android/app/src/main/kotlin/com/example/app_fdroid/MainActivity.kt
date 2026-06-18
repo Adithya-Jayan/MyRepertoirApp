7a1889cedbaf654e1dd6dfd9bce8c2a06b3e26b1
Adithya Jayan <adithyaj8@gmail.com>
2026-06-18 20:22:24 +0530

    Creates separate initial folders for playstore and fdroid builds.

---
+++ app_fdroid/android/app/src/main/kotlin/com/example/app_fdroid/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_fdroid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ app_playstore/android/app/src/main/kotlin/com/example/app_playstore/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_playstore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
